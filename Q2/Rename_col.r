ab9d87fbfbea3ff973098e4aa4e90c7f9b5c4de8
DeepthiThomas <deepthithomaskannan@gmail.com>
2015-02-05 10:47:41 -0700

    added error handling

--- Q2/Rename_col.r
+++ Q2/Rename_col.r
@@ -1,8 +1,32 @@
 #!/usr/bin/env Rscript
 # Rename a column in R
-args<-commandArgs(TRUE)
-data<-read.csv(args[1], header = T)
-names(data)[names(data)=="v1"]<-"normal"
-names(data)[names(data)=="v2"]<-"poisson"
-names(data)[names(data)=="v3"]<-"guass"
-write.csv(data,file='renamedFile.csv')
+args<-commandArgs(TRUE)  #receive file name through command line arg
+#add error handling
+tryCatch(
+{
+assign("file1",data<-read.csv(args[1], header = T),envir=.GlobalEnv) #read file and assign in to a global var
+},
+warning = function(w)
+{
+	print("caused a warning\n")   #messages in case of warning
+	message("Here is the original warning message:")
+	message(w)
+	return(NULL)
+},
+error = function(err)
+{
+print("Could not find the file mentioned in the commandline arguement\n")  #messages in case of error
+message("Here is the original message:")
+message(err)
+return(NA)  #choose a return value in case of error
+},
+finally={
+	print("read file successfully\n")
+}
+)
+names(file1)[names(file1)=="v1"]<-"normal"   #renaming the columns
+names(file1)[names(file1)=="v2"]<-"poisson"
+names(file1)[names(file1)=="v3"]<-"guass"
+write.csv(file1,file=args[1])	#rewrite the file
+
+
